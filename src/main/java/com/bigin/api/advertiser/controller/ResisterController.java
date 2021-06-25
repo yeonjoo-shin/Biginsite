687757145c72f1066fd3b7124282178edda3e5e7
yeonjoo-shin <63095243+yeonjoo-shin@users.noreply.github.com>
2021-06-25 21:43:22 +0900

    기본 api 호출가능

--- src/main/java/com/bigin/api/advertiser/controller/ResisterController.java
+++ src/main/java/com/bigin/api/advertiser/controller/ResisterController.java
@@ -3,11 +3,18 @@ package com.bigin.api.advertiser.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * create 2021/06/25
+ * crud API
+ *
+ * @author yjshin
+ * @since jdk11
+ */
 @RestController
 @RequestMapping(value = "/advertiser")
 public class ResisterController {
 
-    @GetMapping(name = "등록" , value = "/shopdata")
+    @GetMapping(name = "상품 등록" , value = "/shopdata")
     public @ResponseBody Object setShopdataRegister() {
         return "hooew";
     }
