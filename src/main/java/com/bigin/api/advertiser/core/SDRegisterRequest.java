4ea309bf19d17b479cdf83d4af52d39cabe325f6
yeonjoo-shin <63095243+yeonjoo-shin@users.noreply.github.com>
2021-06-26 17:36:46 +0900

    jsonformat

--- src/main/java/com/bigin/api/advertiser/core/SDRegisterRequest.java
+++ src/main/java/com/bigin/api/advertiser/core/SDRegisterRequest.java
@@ -1,5 +1,6 @@
 package com.bigin.api.advertiser.core;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,7 +17,9 @@ public class SDRegisterRequest {
     private int    prdtPrice;   // 상품 가격
     private String prdtImg;     // 상품 이미지
     private String prdtUrl;     // 상품 url
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS", timezone = "Asia/Seoul")
     private Date   regDate;     // 생성일
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS", timezone = "Asia/Seoul")
     private Date   artDate;     // 수정일
 
 }
