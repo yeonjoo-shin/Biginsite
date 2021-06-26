658a0cf8538158376a12d1cb86c88e5fa009d4a5
yeonjoo-shin <63095243+yeonjoo-shin@users.noreply.github.com>
2021-06-26 11:34:39 +0900

    insert완료

--- src/main/java/com/bigin/api/advertiser/repository/DataResisterReository.java
+++ src/main/java/com/bigin/api/advertiser/repository/DataResisterReository.java
@@ -4,10 +4,18 @@ import com.bigin.api.advertiser.core.SDRegisterRequest;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+/**
+ * create on2021/06/25
+ * crud API
+ *
+ * @author yjshin
+ * @since jdk11
+ */
 @Mapper
 @Repository
-public interface DataMapper {
-
-    String d ="dd";
-    public void insertData(SDRegisterRequest params);
+public interface DataResisterReository {
+    /**
+     *    상품 데이터 등록.
+     */
+    void insertData(SDRegisterRequest params);
 }
--- src/main/java/com/bigin/api/advertiser/service/DataRegisterService.java
+++ src/main/java/com/bigin/api/advertiser/service/DataRegisterService.java
@@ -1,37 +1,26 @@
 package com.bigin.api.advertiser.service;
 
 import com.bigin.api.advertiser.core.SDRegisterRequest;
-import com.bigin.api.advertiser.repository.DataMapper;
-import com.bigin.api.advertiser.repository.DataRegisterRepository;
-import lombok.AllArgsConstructor;
+import com.bigin.api.advertiser.repository.DataResisterReository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-
-import java.util.Date;
 
 @Slf4j
 @Service
 @Component
 public class DataRegisterService {
 
-    //private final DataRegisterRepository repository;
-
-
-    /*public DataRegisterService(DataRegisterRepository repository) {
-        this.repository = repository;
-    }*/
-    private final DataMapper mapper;
-    public DataRegisterService(DataMapper mapper) {
-        this.mapper = mapper;
+    private final DataResisterReository reository;
+    public DataRegisterService(DataResisterReository reository) {
+        this.reository = reository;
     }
 
     /**
      *    상품 데이터 등록.
      */
     public void insertShopData(SDRegisterRequest params) {
-        //repository.insertShopData(params);
-        mapper.insertData(params);
+
+        reository.insertData(params);
     }
 }
